ac116c42dc73f7d5c0443c6824c82665f55a29de
tean327 <65708327+tean327@users.noreply.github.com>
2025-12-24 21:59:02 +0100

    Add files via upload

---
+++ Chrome Extension/Content.js
@@ -0,0 +1,53 @@
+const htmlPath = chrome.runtime.getURL('HTMLPage1.html');
+const CssPath = chrome.runtime.getURL('ExtensionStyle.css');
+
+switch (window.location.hostname) {
+    case "www.youtube.com":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+    case "www.bible.com":
+        alert("tié un bon mon frr");
+        break
+    case "x.com":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+    case "www.instagram.com":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+    case "www.facebook.com":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+
+}
