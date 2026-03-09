28c78883ff8901806115199798b64b9fe582dd5c
tean327 <65708327+tean327@users.noreply.github.com>
2026-03-09 13:05:52 +0100

    Add files via upload

---
+++ Chrome Extension/Chrome-Extension/README.md
@@ -0,0 +1,2 @@
+# Chrome Extension
+This little chrome extension was my first experienc with JSON and JavaScript, if I need to descirbe the hardest problem I find out with this project it will be to link my HTML file to the Java Script to change the webpage.
--- Chrome Extension/Content.js
+++ Chrome Extension/Content.js
@@ -1,6 +1,9 @@
 const htmlPath = chrome.runtime.getURL('HTMLPage1.html');
 const CssPath = chrome.runtime.getURL('ExtensionStyle.css');
 
+let blockedWords = ["test", "cs", "guerre", "chatgpt"];
+
+
 switch (window.location.hostname) {
     case "www.youtube.com":
         fetch(htmlPath)
@@ -13,6 +16,9 @@ switch (window.location.hostname) {
             .then(css => {
                 document.head.appendChild = css;
             }); break;
+    case "www.bible.com":
+        alert("tié un bon mon frr");
+        break
     case "x.com":
         fetch(htmlPath)
             .then(response => response.text())
@@ -46,6 +52,94 @@ switch (window.location.hostname) {
             .then(css => {
                 document.head.appendChild = css;
             }); break;
+    case "www.LinkedIn.com":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+    case "www.onlyfans.com":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+    case "www.twitch.tv":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
 
+    case "www.link.me":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+    case "www.x.com":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+    case "www.linktr.ee":
+        fetch(htmlPath)
+            .then(response => response.text())
+            .then(html => {
+                document.body.innerHTML = html;
+            })
+        fetch(CssPath)
+            .then(response => response.text())
+            .then(css => {
+                document.head.appendChild = css;
+            }); break;
+}
 
+//Block certain keywords
+let url = window.location.href;
+if (url.includes("google.com/search") || url.includes("https://fr.search.yahoo.com/")) {
+
+    //word is the index not the word by itself
+    for (var word in blockedWords) {
+        if (url.includes(blockedWords[word], 10)) {
+
+            //redirect
+            fetch(htmlPath)
+                .then(response => response.text())
+                .then(html => {
+                    document.body.innerHTML = html;
+                })
+            fetch(CssPath)
+                .then(response => response.text())
+                .then(css => {
+                    document.head.appendChild = css;
+                });
+        }
+    }
 }
