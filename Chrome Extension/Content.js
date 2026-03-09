5fbdc80fc245af2e7a681efca1351f20e4f4196f
tean327 <65708327+tean327@users.noreply.github.com>
2026-03-09 18:38:25 +0100

    Update Content.js

--- Chrome Extension/Content.js
+++ Chrome Extension/Content.js
@@ -2,86 +2,15 @@ const htmlPath = chrome.runtime.getURL('HTMLPage1.html');
 const CssPath = chrome.runtime.getURL('ExtensionStyle.css');
 
 let blockedWords = ["test", "cs", "guerre", "chatgpt"];
+let blockedSite = ["www.youtube.com", "x.com", "instagram.com", "facebook.com", "www.twitch.tv"];
+
+BlockSites();
+BlockSearch();
 
 
-switch (window.location.hostname) {
-    case "www.youtube.com":
-        fetch(htmlPath)
-            .then(response => response.text())
-            .then(html => {
-                document.body.innerHTML = html;
-            })
-        fetch(CssPath)
-            .then(response => response.text())
-            .then(css => {
-                document.head.appendChild = css;
-            }); break;
-    case "x.com":
-        fetch(htmlPath)
-            .then(response => response.text())
-            .then(html => {
-                document.body.innerHTML = html;
-            })
-        fetch(CssPath)
-            .then(response => response.text())
-            .then(css => {
-                document.head.appendChild = css;
-            }); break;
-    case "www.instagram.com":
-        fetch(htmlPath)
-            .then(response => response.text())
-            .then(html => {
-                document.body.innerHTML = html;
-            })
-        fetch(CssPath)
-            .then(response => response.text())
-            .then(css => {
-                document.head.appendChild = css;
-            }); break;
-    case "www.facebook.com":
-        fetch(htmlPath)
-            .then(response => response.text())
-            .then(html => {
-                document.body.innerHTML = html;
-            })
-        fetch(CssPath)
-            .then(response => response.text())
-            .then(css => {
-                document.head.appendChild = css;
-            }); break;
-    case "www.LinkedIn.com":
-        fetch(htmlPath)
-            .then(response => response.text())
-            .then(html => {
-                document.body.innerHTML = html;
-            })
-        fetch(CssPath)
-            .then(response => response.text())
-            .then(css => {
-                document.head.appendChild = css;
-            }); break;
-    case "www.twitch.tv":
-        fetch(htmlPath)
-            .then(response => response.text())
-            .then(html => {
-                document.body.innerHTML = html;
-            })
-        fetch(CssPath)
-            .then(response => response.text())
-            .then(css => {
-                document.head.appendChild = css;
-            }); break;
-}
-
-//Block certain keywords
-let url = window.location.href;
-if (url.includes("google.com/search") || url.includes("https://fr.search.yahoo.com/")) {
-
-    //word is the index not the word by itself
-    for (var word in blockedWords) {
-        if (url.includes(blockedWords[word], 10)) {
-
-            //redirect
+function BlockSites() {
+    for (var site in blockedSite) {
+        if (window.location.hostname == blockedSite[site]) {
             fetch(htmlPath)
                 .then(response => response.text())
                 .then(html => {
@@ -94,5 +23,32 @@ if (url.includes("google.com/search") || url.includes("https://fr.search.yahoo.c
                 });
         }
     }
-
 }
+
+
+function BlockSearch() {
+
+    //Block certain keywords
+    let url = window.location.href;
+    if (url.includes("google.com/search") || url.includes("https://fr.search.yahoo.com/")) {
+
+        //word is the index not the word by itself
+        for (var word in blockedWords) {
+            if (url.includes(blockedWords[word], 10)) {
+
+                //redirect
+                fetch(htmlPath)
+                    .then(response => response.text())
+                    .then(html => {
+                        document.body.innerHTML = html;
+                    })
+                fetch(CssPath)
+                    .then(response => response.text())
+                    .then(css => {
+                        document.head.appendChild = css;
+                    });
+            }
+        }
+    }
+}
+
